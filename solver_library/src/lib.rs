6455939375065340f788c497518569c653d338ef
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2024-10-13 11:16:47 +0200

    Move WASM bindings to their own file

--- solver_library/src/lib.rs
+++ solver_library/src/lib.rs
@@ -1,32 +1,3 @@
-use wasm_bindgen::prelude::*;
+mod wasm;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-#[wasm_bindgen]
-#[derive(Debug)]
-pub struct RobotPosition {
-    pub x: usize,
-    pub y: usize,
-}
-
-#[wasm_bindgen]
-impl RobotPosition {
-    #[wasm_bindgen(constructor)]
-    pub fn new(x: usize, y: usize) -> RobotPosition {
-        RobotPosition { x, y }
-    }
-
-    // We can't generate WASM bindings for functions of the Display trait!
-    #[wasm_bindgen]
-    pub fn to_string(&self) -> String {
-        format!("{:?}", self)
-    }
-}
+pub use crate::wasm::*;
